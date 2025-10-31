84304ac2b62fbfd824d689db94129796d9bd41c3
dishawaghmare26 <noturacess0226@gmail.com>
2025-10-31 18:14:57 +0530

    Fix formatting issues in logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
 <?php
-// filepath: c:\Users\Zedex\Desktop\Akshay_excel\1_Style_Caret\Chessfloor\api\logout.php
+
 session_start();
 $_SESSION = array();
 session_destroy();
 header("location: ../login.html");
 exit;
+
 ?>
