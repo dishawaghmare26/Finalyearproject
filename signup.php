7a4aa4bb06978735a4ed69df9c6646220dd79e8d
dishawaghmare26 <noturacess0226@gmail.com>
2025-10-31 18:15:31 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-// filepath: c:\Users\Zedex\Desktop\Akshay_excel\1_Style_Caret\Chessfloor\api\signup.php
+
 require_once "config.php";
 
 header('Content-Type: application/json');
@@ -33,4 +33,5 @@ if ($stmt = $conn->prepare($sql)) {
     $stmt->close();
 }
 $conn->close();
+
 ?>
